fca75e6646f7d14101829239307bf2085469e707
marviquint <marvsbahio@gmail.com>
2023-05-03 17:50:49 +0800

    changes in crawling

--- scrapy_login_spider/spiders/loginspider.py
+++ scrapy_login_spider/spiders/loginspider.py
@@ -1,69 +1,84 @@
-import scrapy
+from scrapy import Spider, Request, FormRequest
+from scrapy.selector import Selector
+from scrapy.utils.project import get_project_settings
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import scrapy
 
-class LoginSpider(scrapy.Spider):
-    name = "login"
-    #start_urls = ["page name"]
-    start_urls = ["pagename"]
-    urls = ["pagename"]
+
+class ExampleSpider(scrapy.Spider):
+    name = 'login'
+    allowed_domains = ['page name']
+
+    def __init__(self, username="username", password="password", *args, **kwargs):
+        super(ExampleSpider, self).__init__(*args, **kwargs)
+        self.username = username
+        self.password = password
+        self.driver = webdriver.Chrome(executable_path = "path")
+        self.driver.maximize_window()
 
     def start_requests(self):
-        # Create a new Selenium WebDriver instance
-        #driver = webdriver.Chrome(executable_path = "path")
-        driver = webdriver.Chrome(executable_path = "path")
-        
-        # Navigate to the login page
-        #driver.get("page name")
-        driver.get("pagename")
-        
-        # Fill in the form fields
-        # username_field = driver.find_element("name", "username")
-        # username_field.send_keys("username")
-        # password_field = driver.find_element("name", "password")
-        # password_field.send_keys("password")
+        # Start by logging in
+        self.driver.get("page name")
+        self.driver.find_element("name", "username").send_keys(self.username)
+        self.driver.find_element("name","password").send_keys(self.password)
+        self.driver.find_element(By.XPATH, "//button").click()
 
-
-        username_field = driver.find_element("name", "username")
-        username_field.send_keys("username")
-        password_field = driver.find_element("name", "password")
-        password_field.send_keys("password")
-        
-        # Submit the form
-        submit_button = driver.find_element(By.XPATH, "//button")
-        submit_button.click()
-        
         # Wait for the page to load
-        driver.implicitly_wait(10)
-        
-        # Extract any cookies or tokens needed for subsequent requests
-        cookies = driver.get_cookies()
-        
-        # Send an authenticated request using Scrapy
-        for url in self.start_urls:
-            yield scrapy.Request(url, cookies=cookies, callback=self.parse)
-            
-    def parse(self, response):
-        # Extract data from the authenticated page
-        print("Login Successful!")
+        self.driver.implicitly_wait(10)
 
-        # Follow links to other pages
-    #     links = response.css('.yui-dt-col-title .theme-color-1').getall()
-    #     for link in links:
-    #         child_links = response.css('#yui-rec27 a').getall()
 
-    #         for link in child_links:
-    #             yield response.follow(link, callback=self.parse_link)
-
-    # def parse_link(self, response):
-    #     # Extract data from the link page
-    #     # ...
-    #     # Extract title first
-    #     document_name = '\u000A'.join(response.css('.node-info .dark').css('::text').getall())
-    #     # Extract the rules content
-    #     data = '\u000A'.join(response.css('.form-fields').css('::text').getall())
-    #     print("Data extracted from page: ", document_name, data)
+        # Start crawling from the dashboard
+        yield scrapy.Request(
+            url="page name",
+            callback=self.parse_dashboard,
+        )
+    
         
-    #     # Store the data in an item
-    #     item = {}
-    #     item[''+document_name] = data      
+
+    def parse_dashboard(self, response):
+        # Parse the first anchor tag inside #HEADER_MY_TASKS_text .alfresco-navigation-_HtmlAnchorMixin
+        # from scrapy.shell import inspect_response
+        # inspect_response(response, self)
+        anchor_url = "page name"
+        yield scrapy.Request(url=response.urljoin(anchor_url), callback=self.parse_anchor)
+
+    def parse_anchor(self, response):
+        # Parse the anchor tag inside #insert-taskTitle li:nth-child(1) .filter4
+        anch_url = "page name"
+
+        if anch_url:    
+             yield scrapy.Request(url=response.urljoin(anch_url), callback=self.parse_inner_anchor)
+        else:
+            print("No inner anchor URL found.")
+
+    def parse_inner_anchor(self, response):
+        # Parse the anchor tags inside .yui-dt-col-title .theme-color-1
+        for anchor in response.css(".yui-dt-col-title .theme-color-1"):
+            # Parse the anchor tag with class .view_more_actions inside each anchor tag in .yui-dt-col-title .theme-color-1
+            view_more_url = anchor.css(".view_more_actions::attr(href)").get()
+            yield scrapy.Request(url=response.urljoin(view_more_url), callback=self.parse_details)
+
+        # Follow the next page link if it exists
+        next_page_url = response.css(".yui-pg-next::attr(href)").get()
+        if next_page_url:
+            yield scrapy.Request(url=response.urljoin(next_page_url), callback=self.parse_inner_anchor)
+
+    def parse_details(self, response):
+        # Extract the document title and properties from the page
+        document_title = response.css(".node-info .dark::text").get()
+        properties = response.css(".form-fields ::text").getall()
+
+        # Yield the extracted data
+        yield {
+            "document_title": document_title,
+            "properties": properties,
+        }
+        # Print the extracted data
+        print(f"Title: {document_title}")
+        print(f"Properties: {properties}")
+
+    def close(self, reason):
+        self.driver.quit()
